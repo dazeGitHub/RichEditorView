ed1b1a43e978637c4703c01ffe893f07700bd5b4
zhaoyaze <zhaoyaze@52toys.com>
2021-06-08 17:12:46 +0800

    解决编译报错问题，添加 jitpack

--- app/src/main/java/me/jingbin/richeditorview/MainActivity.java
+++ app/src/main/java/me/jingbin/richeditorview/MainActivity.java
@@ -2,10 +2,11 @@ package me.jingbin.richeditorview;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
